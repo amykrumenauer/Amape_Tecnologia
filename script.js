9d51ec93c6290110fc421fca5202d5a8405a2967
Amanda Krumenauer <krumenaueramy5@gmail.com>
2026-07-27 00:19:27 -0300

    versao 2

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("AMAPE Tecnologia carregada:");
+console.log("AMAPE Tecnologia carregada 🚀");
+
+
+window.addEventListener("scroll",()=>{
+
+
+const header = document.querySelector("header");
+
+
+if(window.scrollY > 50){
+
+header.style.background="#020617f5";
+
+}else{
+
+header.style.background="#020617dd";
+
+}
+
+
+});
